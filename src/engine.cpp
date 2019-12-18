534453647e3d9d40e9461710e8298bc97938b44a
Miles Smith <milesdavidsmith@gmail.com>
2019-12-18 02:27:58 -0800

    glfw test

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,50 +1,28 @@
-#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-#include <SDL2/SDL.h>
-#include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
-    SDL_Window *window;
-    SDL_GLContext gl_context;
-    SDL_Event sdl_event;
+    GLFWwindow *window = nullptr;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        cerr << "cannot initialize sdl video" << endl;
-        return 1;
-    }
-    
-    window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL |
-        SDL_WINDOW_SHOWN);
+    cout << "glfwInit: " << (glfwInit() == GLFW_TRUE ? "yes" : "no") << endl;
 
-    if (!window) {
-        cerr << "cannot open window" << endl;
-        return 1;
-    }
-    
-    gl_context = SDL_GL_CreateContext(window);
+    window = glfwCreateWindow(500, 300, "My Test", NULL, NULL);
 
-    if (!gl_context) {
-        cerr << "cannot create gl context" << endl;
-        return 1;
+    cout << "window: " << window << endl;
+    cout << "context: " << glfwGetCurrentContext() << endl;
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-    if (glewInit() != GLEW_OK) {
-        cerr << "failed to call glewInit" << endl;
-        return 1;
-    }
-
-    cout << "window: " << window << endl
-         << "gl_context: " << gl_context << endl;
-
-    SDL_Delay(3000);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-	return 0;
-
+    return 0;
 }
