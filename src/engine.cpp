13de2e4f275242a47f96df207411c364a54ec01b
Miles Smith <msmith@ucsd.edu>
2019-12-20 15:58:32 -0800

    draw a white triangle

--- src/engine.cpp
+++ src/engine.cpp
@@ -17,13 +17,27 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    //glTexImage2D(GL_TEXTURE2D, 0, GL_RGBA8, 10, 10, 0, GL_RGBA, GL_UNSIGNED_BYTE, ptrToPixelBuf);
+
     cout << "glewInit: " << (glewInit() == GLEW_OK ? "yes" : "no") << endl;
 
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+
+        glEnd();
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glfwTerminate();
 
     return 0;
 }
