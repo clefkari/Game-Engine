73113c2a0f520dc78c4153b9274066177da1339b
Miles Smith <msmith@ucsd.edu>
2019-12-22 22:25:25 -0800

    stuff

--- src/util.h
+++ src/util.h
@@ -29,7 +29,7 @@ GLFWwindow *init() {
 
     // Request OpenGL 4.5 with core profile.
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *window = glfwCreateWindow(640, 480, "test window", NULL, NULL);
