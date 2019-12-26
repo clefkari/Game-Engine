971037212926193d74efb1c7b24353e83981903f
Miles Smith <msmith@ucsd.edu>
2019-12-26 00:42:56 -0800

    more stuff with vaos and error handling

--- src/engine.cpp
+++ src/engine.cpp
@@ -25,11 +25,13 @@ int main() {
     if (!program_id) return 0;
     WRAP_GL(glUseProgram(program_id));
 
-    float verts_a[] = {0,  0,    1,  1,   -1,  1};
-    float verts_b[] = {0,  0,    1,  -1,   -1,  -1};
+    float verts_a[] = {
+        0, 0,
+        1, 1,
+        -1, 1};
 
-    const int num_vaos = 2, num_vbos = 2;
-    GLuint vaos[num_vaos], vbos[num_vbos];
+    const int num_vaos = 2, num_vbos = 2, num_ibos = 1;
+    GLuint vaos[num_vaos], vbos[num_vbos], ibos[num_ibos];
 
     WRAP_GL( glGenVertexArrays(num_vaos, vaos) );
     WRAP_GL( glGenBuffers(num_vbos, vbos) );
@@ -40,24 +42,14 @@ int main() {
     WRAP_GL( glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0) );
     WRAP_GL( glBufferData(GL_ARRAY_BUFFER, sizeof(verts_a), verts_a, GL_STATIC_DRAW) );
 
-
-    WRAP_GL( glBindVertexArray(vaos[1]) );
-    WRAP_GL( glBindBuffer(GL_ARRAY_BUFFER, vbos[1]) );
-    WRAP_GL( glEnableVertexAttribArray(0) );
-    WRAP_GL( glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0) );
-    WRAP_GL( glBufferData(GL_ARRAY_BUFFER, sizeof(verts_b), verts_b, GL_STATIC_DRAW) );
+    WRAP_GL( glGenBuffers(num_ibos, ibos) );
+    WRAP_GL( glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibos[0]) );
 
     int i = 0;
     int j = 0;
 
     while (!glfwWindowShouldClose(window)) {
         WRAP_GL(glClear(GL_COLOR_BUFFER_BIT));
-
-        if (i++%50 == 0) {
-            int b = j++ % 2;
-            WRAP_GL( glBindVertexArray(vaos[b]) );
-            WRAP_GL( glBindBuffer(GL_ARRAY_BUFFER, vbos[b]) );
-        }
 
         WRAP_GL( glDrawArrays(GL_TRIANGLES, 0, 3) );
 
