4c7d72227a0cbb7e5ecc10931ac0b326d2b462bc
Miles Smith <msmith@ucsd.edu>
2019-12-26 03:17:52 -0800

    drawing a square with index buffers

--- src/fragment-shader.glsl
+++ src/fragment-shader.glsl
@@ -4,5 +4,5 @@ in vec4 position;
 out vec4 fColor;
 
 void main() {
-    fColor = vec4(sin(position.x), cos(position.y), 1);
+    fColor = vec4(tan(position.x*position.y*90), tan(position.x*position.y*20), sin(30*position.y*position.x), 1);
 }
